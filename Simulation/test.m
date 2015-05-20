da7515cf685d1ebafd367f6c7ba9428a675ba965
Sander <sander@groowup.nl>
2015-05-20 12:18:44 +0200

    Commentaar toegevoegd over de parameters.

--- Simulation/test.m
+++ Simulation/test.m
@@ -3,9 +3,9 @@ clear all;
 close all;
 
 %% INIT
-PAGECNT = 100;
-TUPLECNT = 10;
-SELECTIVITY = .2;
+PAGECNT = 100; %Totale aantal pagina's
+TUPLECNT = 10; %Aantal tuples per pagina
+SELECTIVITY = .2; %Percentage (tussen 0...1) van de tuples die aan de query zouden voldoen 
 
 %# Fill the array with 0 and 1 and reorder
 Data = [ones(TUPLECNT,(SELECTIVITY*PAGECNT)) zeros(TUPLECNT,((1-SELECTIVITY)*PAGECNT))];        
