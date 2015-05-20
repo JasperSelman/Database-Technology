efde67dcad7df206f560b072b51050821630dcf1
hahschaa <hah.vanschaaijk@outlook.com>
2015-05-20 21:16:38 +0200

    Simulation update

--- Simulation/test.m
+++ Simulation/test.m
@@ -3,9 +3,9 @@ clear all;
 close all;
 
 %% INIT
-PAGECNT = 100000; %Totale aantal pagina's
-TUPLECNT = 1000; %Aantal tuples per pagina
-SELECTIVITY = .2; %Percentage (tussen 0...1) van de tuples die aan de query zouden voldoen 
+PAGECNT = 100; %Totale aantal pagina's
+TUPLECNT = 10; %Aantal tuples per pagina
+SELECTIVITY = .10; %Percentage (tussen 0...1) van de tuples die aan de query zouden voldoen 
 
 %# Fill the array with 0 and 1 and reorder
 Data = [ones(TUPLECNT,(SELECTIVITY*PAGECNT)) zeros(TUPLECNT,((1-SELECTIVITY)*PAGECNT))];        
