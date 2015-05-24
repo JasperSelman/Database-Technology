7d64ee3db0e799d395fe8f22bbae1fc720bd7e65
RConijn <m.a.conijn@student.tue.nl>
2015-05-24 15:46:11 +0200

    update test.m

--- Simulation/test.m
+++ Simulation/test.m
@@ -5,18 +5,31 @@ close all;
 %% INIT
 PAGECNT = 100; %Totale aantal pagina's
 TUPLECNT = 10; %Aantal tuples per pagina
-SELECTIVITY = .10; %Percentage (tussen 0...1) van de tuples die aan de query zouden voldoen 
+SELECTIVITIES = [.01 .02 .05 .1 .2 .4 .6 .8 1];%Percentage (tussen 0...1) van de tuples die aan de query zouden voldoen 
+fsPenalty = zeros(1,length(SELECTIVITIES));
+isPenalty = zeros(1,length(SELECTIVITIES));
 
-%# Fill the array with 0 and 1 and reorder
-Data = [ones(TUPLECNT,(SELECTIVITY*PAGECNT)) zeros(TUPLECNT,((1-SELECTIVITY)*PAGECNT))];        
-Data(randperm(numel(Data))) = Data;
+for i=1:length(SELECTIVITIES)
+    SELECTIVITY = SELECTIVITIES(i);
+    %# Fill the array with 0 and 1 and reorder
+    Data = [ones(TUPLECNT,(SELECTIVITY*PAGECNT)) zeros(TUPLECNT,((1-SELECTIVITY)*PAGECNT))];        
+    Data(randperm(numel(Data))) = Data;
 
-%% RUN a full scan on the data above
-fs = FullScan(Data);
-fs.scan();
-fs
+    %% RUN a full scan on the data above
+    fs = FullScan(Data);
+    fs.scan();
+    fs
+    fsPenalty(i)= fs.returnPenalty;
+    clear fs;
 
-%% RUN an index scan on the data above
-is = IndexScan(Data);
-is.scan();
-is
+    %% RUN an index scan on the data above
+    is = IndexScan(Data);
+    is.scan();
+    is
+    isPenalty(i)= is.returnPenalty;
+    clear is;
+
+end
+
+fsPenalty
+isPenalty
