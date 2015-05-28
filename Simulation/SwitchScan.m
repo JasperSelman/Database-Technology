38df40e440d85b6037b2b5f6116d3769ef8004ed
hahschaa <hah.vanschaaijk@outlook.com>
2015-05-28 12:39:06 +0200

    Update SwitchScan
    only index -> full

--- Simulation/SwitchScan.m
+++ Simulation/SwitchScan.m
@@ -1,6 +1,6 @@
 classdef SwitchScan < Scan & IndexScan & FullScan & handle
     properties
-        treshold = 30;
+        threshold = 0.01;
     end      
 
     methods
@@ -9,16 +9,17 @@ classdef SwitchScan < Scan & IndexScan & FullScan & handle
         end
         
         function switchscan(obj, card)
+            exp_selectivity = floor(numel(obj.Data) / card);
             curr_card = 0;
             
-            if(card < obj.threshold)
-                %Index Scan
+            if(exp_selectivity < obj.threshold)
+                %Index Scan 
                 for i = 1 : numel(obj.Data)
+                    if(curr_card > card)
+                        break;
+                    end
                     if(obj.Data(i) == 1)
                         curr_card = curr_card + 1;
-                        if(curr_card > obj.treshold)
-                            break;
-                        end
                         obj.randomPagePenalty = obj.randomPagePenalty + 1;
                         obj.returnPenalty = obj.returnPenalty + 1; 
                         cnt = cnt + 1;
@@ -30,44 +31,29 @@ classdef SwitchScan < Scan & IndexScan & FullScan & handle
                 end
 
                 %Full scan
+                %first access is random
+                obj.sequentialPagePenalty = obj.sequentialPagePenalty - 1;
+                obj.randomPagePenalty = obj.randomPagePenalty + 1;
+
+                %For each column in Data (which are the pages)
                 for i = 1 : size(obj.Data,2)
                     obj.sequentialPagePenalty = obj.sequentialPagePenalty + 1;
                     column = obj.Data(:,i);
+                    %For each tuple in page
                     for j = 1 : size(column,1)
                        if(column(j) == 1)
                           obj.returnPenalty = obj.returnPenalty + 1; 
                        end
                     end               
                 end
+                
             else
-                %Full scan
-                for i = 1 : size(obj.Data,2)
-                    obj.sequentialPagePenalty = obj.sequentialPagePenalty + 1;
-                    column = obj.Data(:,i);
-                    for j = 1 : size(column,1)
-                        if(column(j) == 1)
-                            curr_card = curr_card + 1;
-                            if(curr_card > obj.treshold)
-                                break;
-                            end
-                            obj.returnPenalty = obj.returnPenalty + 1; 
-                       end
-                    end               
-                end
+                %Full Scan
+                
                 %Index Scan
-                for i = 1 : numel(obj.Data)
-                    if(obj.Data(i) == 1)
-                        curr_card = curr_card + 1;
-                        obj.randomPagePenalty = obj.randomPagePenalty + 1;
-                        obj.returnPenalty = obj.returnPenalty + 1; 
-                        cnt = cnt + 1;
-                        if(cnt == obj.Ps)
-                            obj.sequentialPagePenalty = obj.sequentialPagePenalty + 1;
-                            cnt = 0;
-                        end
-                    end                
-                end             
+                %superclass call
             end
+           
             
         end
         
