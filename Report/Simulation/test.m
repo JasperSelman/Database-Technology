f7ff052972db743bf9b3b13b4f580f11ce7853e9
RConijn <m.a.conijn@student.tue.nl>
2015-06-10 11:03:42 +0200

    extra figuren kleinere schaal

--- Report/Simulation/test.m
+++ Report/Simulation/test.m
@@ -5,13 +5,13 @@ close all;
 %% INIT
 PAGECNT = 1000000; %Totale aantal pagina's
 TUPLECNT = 10; %Aantal tuples per pagina
-SELECTIVITIES = [.25 .2 .15 .1 .05 .02 .01 .005 .002 .001];%Percentage (tussen 0...1) van de tuples die aan de query zouden voldoen 
+SELECTIVITIES = [.02 .015 .01 .009 .008 .007 .006 .005 .004 .003 .002 .001 .0005 .0001];%Percentage (tussen 0...1) van de tuples die aan de query zouden voldoen 
 randomfactor = 20; %Penalty Random vs Sequential is 10:1
 fsPenaltySeq = zeros(1,length(SELECTIVITIES));
 isPenaltySeq = zeros(1,length(SELECTIVITIES));
 fs.randomPagePenalty = zeros(1,length(SELECTIVITIES));
 is.randomPagePenalty = zeros(1,length(SELECTIVITIES));
-cardinality = 0.005*PAGECNT*TUPLECNT; %for random factor 20 from selectivity > 0.005 full scan performs better
+cardinality = 0.010*PAGECNT*TUPLECNT; %for random factor 20 from selectivity > 0.005 full scan performs better
 
 for i=1:length(SELECTIVITIES)
     i
